7a6939e22af7b448c7b1d6ed4bcca2ec97c4b188
AbdullahBoreny <abdullah55boreny@gmail.com>
2025-12-17 15:37:41 +0300

    fetching text from api

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,26 +1,36 @@
-import {  useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import "./Card.css";
 import Logo from "../assets/react.svg";
 import axios from "axios";
 
-export default function Card() {
-  const [score, setScore] = useState(0);
+export default function Card({ index }) {
   const [img, setImg] = useState("");
-  const [text, setText] = useState("")
+  const [text, setText] = useState("");
   const fetchImage = async () => {
-    await axios.get("https://pokeapi.co/api/v2/pokemon/1")
-    .then(response => setImg(response.data.sprites.front_default))
+    await axios
+      .get(`https://pokeapi.co/api/v2/pokemon/${index}`)
+      .then((response) => setImg(response.data.sprites.front_default));
   };
-   
+  const fetchText = async () => {
+    await axios
+      .get(`https://pokeapi.co/api/v2/pokemon/${index}`)
+      .then((response) => setText(response.data.name));
+  };
+
   useEffect(() => {
-   fetchImage()
-   
-  },[]);
+    fetchImage();
+    fetchText();
+  }, []);
 
   return (
-    <div className="card-container">
-      {img && <img src={img} className="card-image"/>}
-      <div className="image-info">test</div>
+    <div
+      onClick={() => {
+        console.log({ index });
+      }}
+      className="card-container"
+    >
+      {img && <img src={img} className="card-image" />}
+      <div className="image-info">{text ? text : "?"}</div>
     </div>
   );
 }
